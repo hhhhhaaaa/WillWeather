d36c4c7d83c2efe09d672859ad9dd2cd8f4d2d38
Doug <hathemagicpenguin@gmail.com>
2021-03-06 10:43:13 -0500

    API Fixing (#4)
    Overview 
    In the process of adding an API.
    
    HTML/CSS
    Linked jquery.
    
    Javascript
    Requires initiation of api.js
    Requires api keys.
    
    Repository
    Added api.js
    Fixed .gitignore to block api.js

---
+++ public/index.js
@@ -0,0 +1,20 @@
+import {apiKeyWeather, apiKeyGoogles} from "./api.js";
+
+let cityName = "Detroit"
+
+// function mapGet () {
+//     $.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Detroit&inputtype=textquery&key=${apiKeyGoogles}`, data =>{
+//         console.log(data);
+//     });
+// }
+
+// mapGet();
+
+// function weatherGet () {
+//     $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`, data => {
+//         console.log(data);
+//     });
+
+// }
+
+// weatherGet();
